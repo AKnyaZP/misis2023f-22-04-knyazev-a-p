00c0626aff9b3ed2097bdc3db2b68b287fd63fcf
knyazev_a_p <knyazev_artem@icloud.com>
2023-11-22 09:52:59 +0300

    delete dialog

--- scan_gui/scan_gui.cpp
+++ scan_gui/scan_gui.cpp
@@ -1,131 +1,64 @@
-﻿//#include "scan_gui.h"
-//#include <iostream>
-//
-//int main() {
-//    if (!glfwInit())
-//        return -1;
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-//    GLFWwindow* window = glfwCreateWindow(1920, 1080, "DocScan", NULL, NULL);
-//    if (window == NULL)
-//        return 1;
-//    glfwMakeContextCurrent(window);
-//    gl3wInit();
-//    glfwSwapInterval(1);
-//    gladLoadGL();
-//    IMGUI_CHECKVERSION();
-//    ImGui::CreateContext();
-//    ImGui_ImplOpenGL3_Init("#version 330");
-//    ImGui_ImplGlfw_InitForOpenGL(window, true);
-//    ImGuiIO& io = ImGui::GetIO();
-//    (void)io;
-//
-//    ImGuiWindowFlags windowflag = 0;
-//    windowflag |= ImGuiWindowFlags_NoBackground;
-//    windowflag |= ImGuiWindowFlags_NoTitleBar;
-//    windowflag |= ImGuiWindowFlags_MenuBar;
-//    windowflag |= ImGuiWindowFlags_NoBringToFrontOnFocus;
-//    io.FontGlobalScale = 2.0f;
-//
-//    while (!glfwWindowShouldClose(window)) {
-//        glfwPollEvents();
-//        ImGui_ImplOpenGL3_NewFrame();
-//        ImGui_ImplGlfw_NewFrame();
-//        ImGui::NewFrame();
-//        ImGui::SetNextWindowPos({ 0, 0 });
-//        ImGui::SetNextWindowSize({ 1920, 1080 });
-//
-//        ImGui::Begin("DocScan", NULL, windowflag);
-//        if (ImGui::BeginMenuBar()) {
-//            if (ImGui::MenuItem("Open...", "Ctrl+O")) {
-//                std::cout << "open explorer" << std::endl;
-//            }
-//            if (ImGui::MenuItem("Close", "Ctrl+E")) {
-//                return 0;
-//            }
-//        }
-//        if ((ImGui::IsKeyDown(ImGuiKey_RightCtrl) || ImGui::IsKeyDown(ImGuiKey_LeftCtrl)) && ImGui::IsKeyDown(ImGuiKey_O)) {
-//            std::cout << "open explorer" << std::endl;
-//        }
-//        if ((ImGui::IsKeyDown(ImGuiKey_RightCtrl) || ImGui::IsKeyDown(ImGuiKey_LeftCtrl)) && ImGui::IsKeyDown(ImGuiKey_E)) {
-//            return 0;
-//        }
-//
-//        ImGui::End();
-//        ImGui::Render();
-//        int display_w, display_h;
-//        glfwGetFramebufferSize(window, &display_w, &display_h);
-//        glViewport(0, 0, display_w, display_h);
-//        glClearColor(0.f, 0.f, 0.3f, 1.0f); 
-//        glClear(GL_COLOR_BUFFER_BIT);
-//        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-//        glfwSwapBuffers(window);
-//    }
-//
-//    ImGui_ImplOpenGL3_Shutdown();
-//    ImGui_ImplGlfw_Shutdown();
-//    ImGui::DestroyContext();
-//    glfwDestroyWindow(window);
-//    glfwTerminate();
-//    return 0;
-//}
+﻿#include <scan_gui.h>
 
-#include <scan_gui.h>
-
-
-int main() {
-    // Initialize GLFW
+int main()
+{
+    ImFont* pFont = io.Fonts->AddFontFromFileTTF("Roboto-Regular.ttf",
+        10.0f, NULL,
+        ImGui::GetIO().Fonts->GetGlyphRangesCyrillic());
+    // Инициализация GLFW
     glfwInit();
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui Example", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1200, 800, "ScanGui", nullptr, nullptr);
     glfwMakeContextCurrent(window);
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    // Initialize ImGui context
+    // Инициализация ImGui
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
 
-    // Setup platform/renderer bindings
+    // Инициализация стилей и платформы
+    ImGui::StyleColorsDark();
     ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 130");
+    ImGui_ImplOpenGL3_Init("#version 330");
 
-    // Main loop
-    while (!glfwWindowShouldClose(window)) {
-        // Poll events
+    // Главный цикл приложения
+    while (!glfwWindowShouldClose(window))
+    {
         glfwPollEvents();
 
-        // Start new frame
+        // Начало нового кадра ImGui
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // Create GUI
-        ImGui::Begin("MyWindow");
-        ImGui::Text("Hello, world!");
+        // Создание окна ImGui
+        ImGui::Begin("Choose a picture to work with");
+
+        // Добавление содержимого в окно
+
+        ImGui::Text(u8"текст");
+
+        if (ImGui::Button("Click on me!")) {
+            ImGui::Text("The button is pressed");
+        }
         ImGui::End();
 
-        // Render
-        ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+        // Рендеринг окна ImGui
+        glClearColor(0.00f, 0.00f, 0.00f, 0.00f);
         glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
     }
 
-    // Cleanup
+    // Освобождение памяти и завершение работы
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
-    // Cleanup GLFW
     glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0;
 }
-
--- scan_gui/scan_gui.h
+++ scan_gui/scan_gui.h
@@ -1,16 +1,9 @@
 ﻿// scan_gui.h : Include file for standard system include files,
 // or project specific include files.
 
-#pragma once
-
-#define IMGUI_DEFINE_MATH_OPERATORS
-#include "imgui.h"
-#include <iostream>
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-#define USE_IMGUI_TABLES  
-#include<glad/glad.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <GLFW/glfw3.h>
-#include <GL/gl3w.h>
-#include "dialog/ImGuiFileDialog/ImGuiFileDialog.h"
+//#include "dialog/ImGuiFileDialog/ImGuiFileDialog.h"
 // TODO: Reference additional headers your program requires here.
