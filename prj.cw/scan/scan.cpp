cb6492d6bf53c980a0dd88653aff733e07574218
Knayzev Artem <113966078+AKnyaZP@users.noreply.github.com>
2024-01-13 13:29:36 +0300

    Update scan.cpp

--- prj.cw/scan/scan.cpp
+++ prj.cw/scan/scan.cpp
@@ -1,10 +1,10 @@
-﻿#include <include/scan/scan.h>
-#include <Windows.h>
-#include <cstdlib>
-int main() {
+//﻿#include <include/scan/scan.h>
+//#include <Windows.h>
+//#include <cstdlib>
+//int main() {
 	//setlocale(LC_ALL, "Russian");
-	Image image("C:/Users/knyaz_ayotgwn/source/repos/misis2023f-22-04-knyazev-a-p/prj.cw/scan/images_for_scan/example.jpg");
-	image.image_scan();
-	image.image_ocr();
-	return 0;
-}     
+	//Image image("C:/Users/knyaz_ayotgwn/source/repos/misis2023f-22-04-knyazev-a-p/prj.cw/scan/images_for_scan/example.jpg");
+	//image.image_scan();
+	//image.image_ocr();
+	//return 0;
+//}     
